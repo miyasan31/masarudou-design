540e0eca4cbfec32595efe79a9ffa53e254d9ff9
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-17 18:06:23 +0900

    🎉  環境構築

---
+++ svelte.config.js
@@ -0,0 +1,28 @@
+import path from 'path';
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+
+		vite: {
+			resolve: {
+				alias: {
+					$src: path.resolve('./src'),
+					$components: path.resolve('./src/components')
+				}
+			}
+		}
+	}
+};
+
+export default config;
