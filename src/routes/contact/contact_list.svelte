89faaef6d7e0ccba122ad3861504932e589e6080
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-17 18:07:17 +0900

    ✨ page/contact

---
+++ src/routes/contact/contact_list.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+	import Layout from '$components/layout/contact/Layout.svelte';
+</script>
+
+<Layout>
+	<main class="flex flex-1 flex-col items-center overflow-y-scroll">
+		<div class="flex-1 overflow-x-auto w-full pb-40">
+			<table class="table w-full">
+				<thead>
+					<tr>
+						<th>予約番号</th>
+						<th>予約人数</th>
+						<th>予約日時</th>
+						<th />
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th>12</th>
+						<th>2</th>
+						<td>2021/12/01 10:00</td>
+						<td class="text-center w-24">
+							<form action="/SD/servlet/shift_edit" method="POST">
+								<input type="hidden" name="SHIFT_ID" value="" />
+								<button
+									class="
+                    text-white
+                    bg-gray-400
+                    border-0
+                    py-1
+                    px-4
+                    focus:outline-none
+                    hover:bg-gray-400
+                    rounded-full
+                  "
+								>
+									詳細
+								</button>
+							</form>
+						</td>
+					</tr>
+					<tr>
+						<th>13</th>
+						<th>5</th>
+						<td>2021/12/01 10:00</td>
+						<td class="text-center w-24">
+							<form action="/SD/servlet/shift_edit" method="POST">
+								<input type="hidden" name="SHIFT_ID" value="" />
+								<button
+									class="
+                    text-white
+                    bg-gray-400
+                    border-0
+                    py-1
+                    px-4
+                    focus:outline-none
+                    hover:bg-gray-400
+                    rounded-full
+                  "
+								>
+									詳細
+								</button>
+							</form>
+						</td>
+					</tr>
+					<tr>
+						<th>14</th>
+						<th>3</th>
+						<td>2021/12/01 10:00</td>
+						<td class="text-center w-24" />
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</main>
+</Layout>
---
+++ src/routes/contact/contact_register.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import Layout from '$components/layout/contact/Layout.svelte';
+</script>
+
+<Layout>
+	<main class="flex flex-1 flex-col items-center overflow-scroll">
+		<div class="flex flex-col gap-2 w-2/3 max-w-sm">
+			<h1
+				class="
+								pt-8
+								text-center text-2xl
+								md:text-3xl
+								font-medium
+								text-gray-800
+							"
+			>
+				予約登録
+			</h1>
+
+			<label for="employee_id" class="pl-1 text-gray-500">氏名</label>
+
+			<input type="text" value="春 太郎" class="input input-info input-bordered w-full" />
+
+			<input type="hidden" name="EMPLOYEE_ID" value="employee_id" />
+
+			<button
+				class="
+								w-full
+								mx-auto
+								text-white
+								bg-amber-400
+								border-0
+								py-3
+								px-8
+								mt-2
+								focus:outline-none
+								hover:bg-amber-500
+								text-lg
+								rounded-full
+							"
+			>
+				出勤
+			</button>
+		</div>
+	</main>
+</Layout>
