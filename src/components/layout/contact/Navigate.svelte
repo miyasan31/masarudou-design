bbb16d8b1f6b387c03d5dd8f7f900c1c3df05fce
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-17 18:08:02 +0900

    ✨ layout/contact

---
+++ src/components/layout/contact/Navigate.svelte
@@ -0,0 +1,17 @@
+<nav class="w-1/6 min-w-max border-r border-gray-200">
+	<div class="flex flex-col gap-2 p-4">
+		<a href="/contact/contact_list" class="py-2 px-6 hover:bg-amber-100 font-bold rounded-full">
+			予約一覧
+		</a>
+
+		<a href="/contact/contact_register" class="py-2 px-6 hover:bg-amber-100 font-bold rounded-full">
+			予約登録
+		</a>
+
+		<hr class="text-gray-300" />
+
+		<a href="/navigate" class="py-2 px-6 hover:bg-amber-100 font-bold rounded-full"> アプリ選択 </a>
+
+		<a href="/" class="py-2 px-6 hover:bg-amber-100 font-bold rounded-full"> サインアウト </a>
+	</div>
+</nav>
