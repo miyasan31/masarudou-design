f62d367d21459181facc0ea7977dd9d7167c8edb
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-18 14:44:46 +0900

    🎨 format:code

--- svelte.config.js
+++ svelte.config.js
@@ -4,25 +4,25 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter(),
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
 
-		vite: {
-			resolve: {
-				alias: {
-					$src: path.resolve('./src'),
-					$components: path.resolve('./src/components')
-				}
-			}
-		}
-	}
+    vite: {
+      resolve: {
+        alias: {
+          $src: path.resolve('./src'),
+          $components: path.resolve('./src/components'),
+        },
+      },
+    },
+  },
 };
 
 export default config;
