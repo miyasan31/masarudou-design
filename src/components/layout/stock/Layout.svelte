111920d0ce564ed86decf294a0a1a6efa35f3567
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-19 11:53:38 +0900

    ✨

--- src/components/layout/stock/Layout.svelte
+++ src/components/layout/stock/Layout.svelte
@@ -1,7 +1,7 @@
 <div class="flex flex-col w-full h-screen">
   <div class="flex flex-col w-full">
     <header class="flex items-center justify-between w-full bg-blue-400 py-4 px-4 border-b border-gray-200">
-      <h1 class="text-2xl pl-8 font-bold text-white">在庫管理システム</h1>
+      <h1 class="text-2xl pl-8 font-bold text-white">在庫管理</h1>
       <a href="/stock/stock_register" class="btn btn-info">商品登録</a>
     </header>
   </div>
