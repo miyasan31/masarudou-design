c2bf318712b07b63044b0ac1676c299799f408b3
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-19 11:56:45 +0900

    💄

--- src/routes/order/menu_list.svelte
+++ src/routes/order/menu_list.svelte
@@ -3,113 +3,113 @@ import Layout from '$components/layout/order/Layout.svelte';
 </script>
 
 <div class="flex w-full">
-  <div class="container px-5 mx-auto h-screen">
+  <div class="container h-screen px-5 mx-auto overflow-y-auto">
     <h1 class="py-4 text-lg font-bold">フードメニュー</h1>
     <div class="flex flex-wrap -m-2">
-      <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+      <div class="w-full p-2 lg:w-1/3 md:w-1/2">
+        <div class="flex items-center h-full p-4 border border-gray-200 rounded-lg">
           <img
             alt="team"
-            class="w-20 h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded mr-4"
+            class="flex-shrink-0 object-cover object-center w-20 h-20 mr-4 bg-gray-100 rounded"
             src="https://dummyimage.com/80x80" />
           <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">ご飯</h2>
+            <h2 class="font-medium text-gray-900 title-font">ご飯</h2>
             <p class="text-gray-500">300円</p>
           </div>
         </div>
       </div>
-      <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+      <div class="w-full p-2 lg:w-1/3 md:w-1/2">
+        <div class="flex items-center h-full p-4 border border-gray-200 rounded-lg">
           <img
             alt="team"
-            class="w-20 h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded mr-4"
+            class="flex-shrink-0 object-cover object-center w-20 h-20 mr-4 bg-gray-100 rounded"
             src="https://dummyimage.com/84x84" />
           <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">焼肉</h2>
+            <h2 class="font-medium text-gray-900 title-font">焼肉</h2>
             <p class="text-gray-500">300円</p>
           </div>
         </div>
       </div>
-      <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+      <div class="w-full p-2 lg:w-1/3 md:w-1/2">
+        <div class="flex items-center h-full p-4 border border-gray-200 rounded-lg">
           <img
             alt="team"
-            class="w-20 h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded mr-4"
+            class="flex-shrink-0 object-cover object-center w-20 h-20 mr-4 bg-gray-100 rounded"
             src="https://dummyimage.com/88x88" />
           <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">天ぷら</h2>
+            <h2 class="font-medium text-gray-900 title-font">天ぷら</h2>
             <p class="text-gray-500">300円</p>
           </div>
         </div>
       </div>
-      <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+      <div class="w-full p-2 lg:w-1/3 md:w-1/2">
+        <div class="flex items-center h-full p-4 border border-gray-200 rounded-lg">
           <img
             alt="team"
-            class="w-20 h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded mr-4"
+            class="flex-shrink-0 object-cover object-center w-20 h-20 mr-4 bg-gray-100 rounded"
             src="https://dummyimage.com/90x90" />
           <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">餃子</h2>
+            <h2 class="font-medium text-gray-900 title-font">餃子</h2>
             <p class="text-gray-500">300円</p>
           </div>
         </div>
       </div>
-      <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+      <div class="w-full p-2 lg:w-1/3 md:w-1/2">
+        <div class="flex items-center h-full p-4 border border-gray-200 rounded-lg">
           <img
             alt="team"
-            class="w-20 h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded mr-4"
+            class="flex-shrink-0 object-cover object-center w-20 h-20 mr-4 bg-gray-100 rounded"
             src="https://dummyimage.com/94x94" />
           <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">ラーメン</h2>
+            <h2 class="font-medium text-gray-900 title-font">ラーメン</h2>
             <p class="text-gray-500">300円</p>
           </div>
         </div>
       </div>
-      <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+      <div class="w-full p-2 lg:w-1/3 md:w-1/2">
+        <div class="flex items-center h-full p-4 border border-gray-200 rounded-lg">
           <img
             alt="team"
-            class="w-20 h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded mr-4"
+            class="flex-shrink-0 object-cover object-center w-20 h-20 mr-4 bg-gray-100 rounded"
             src="https://dummyimage.com/98x98" />
           <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">かまぼこ</h2>
+            <h2 class="font-medium text-gray-900 title-font">かまぼこ</h2>
             <p class="text-gray-500">300円</p>
           </div>
         </div>
       </div>
-      <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+      <div class="w-full p-2 lg:w-1/3 md:w-1/2">
+        <div class="flex items-center h-full p-4 border border-gray-200 rounded-lg">
           <img
             alt="team"
-            class="w-20 h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded mr-4"
+            class="flex-shrink-0 object-cover object-center w-20 h-20 mr-4 bg-gray-100 rounded"
             src="https://dummyimage.com/100x90" />
           <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">なんちゃらパスタ</h2>
+            <h2 class="font-medium text-gray-900 title-font">なんちゃらパスタ</h2>
             <p class="text-gray-500">300円</p>
           </div>
         </div>
       </div>
-      <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+      <div class="w-full p-2 lg:w-1/3 md:w-1/2">
+        <div class="flex items-center h-full p-4 border border-gray-200 rounded-lg">
           <img
             alt="team"
-            class="w-20 h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded mr-4"
+            class="flex-shrink-0 object-cover object-center w-20 h-20 mr-4 bg-gray-100 rounded"
             src="https://dummyimage.com/104x94" />
           <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">ポテト</h2>
+            <h2 class="font-medium text-gray-900 title-font">ポテト</h2>
             <p class="text-gray-500">300円</p>
           </div>
         </div>
       </div>
-      <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+      <div class="w-full p-2 lg:w-1/3 md:w-1/2">
+        <div class="flex items-center h-full p-4 border border-gray-200 rounded-lg">
           <img
             alt="team"
-            class="w-20 h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded mr-4"
+            class="flex-shrink-0 object-cover object-center w-20 h-20 mr-4 bg-gray-100 rounded"
             src="https://dummyimage.com/108x98" />
           <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">まさる堂気まぐれメニュー</h2>
+            <h2 class="font-medium text-gray-900 title-font">まさる堂気まぐれメニュー</h2>
             <p class="text-gray-500">300円</p>
           </div>
         </div>
@@ -118,74 +118,74 @@ import Layout from '$components/layout/order/Layout.svelte';
     <!-- ----------------------------------------------------------------------- -->
     <h1 class="py-4 text-lg font-bold">ドリンクメニュー</h1>
     <div class="flex flex-wrap -m-2">
-      <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+      <div class="w-full p-2 lg:w-1/3 md:w-1/2">
+        <div class="flex items-center h-full p-4 border border-gray-200 rounded-lg">
           <img
             alt="team"
-            class="w-20 h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded mr-4"
+            class="flex-shrink-0 object-cover object-center w-20 h-20 mr-4 bg-gray-100 rounded"
             src="https://dummyimage.com/80x80" />
           <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">コーラ</h2>
+            <h2 class="font-medium text-gray-900 title-font">コーラ</h2>
             <p class="text-gray-500">300円</p>
           </div>
         </div>
       </div>
-      <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+      <div class="w-full p-2 lg:w-1/3 md:w-1/2">
+        <div class="flex items-center h-full p-4 border border-gray-200 rounded-lg">
           <img
             alt="team"
-            class="w-20 h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded mr-4"
+            class="flex-shrink-0 object-cover object-center w-20 h-20 mr-4 bg-gray-100 rounded"
             src="https://dummyimage.com/84x84" />
           <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">ビール</h2>
+            <h2 class="font-medium text-gray-900 title-font">ビール</h2>
             <p class="text-gray-500">300円</p>
           </div>
         </div>
       </div>
-      <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+      <div class="w-full p-2 lg:w-1/3 md:w-1/2">
+        <div class="flex items-center h-full p-4 border border-gray-200 rounded-lg">
           <img
             alt="team"
-            class="w-20 h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded mr-4"
+            class="flex-shrink-0 object-cover object-center w-20 h-20 mr-4 bg-gray-100 rounded"
             src="https://dummyimage.com/88x88" />
           <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">焼酎</h2>
+            <h2 class="font-medium text-gray-900 title-font">焼酎</h2>
             <p class="text-gray-500">300円</p>
           </div>
         </div>
       </div>
-      <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+      <div class="w-full p-2 lg:w-1/3 md:w-1/2">
+        <div class="flex items-center h-full p-4 border border-gray-200 rounded-lg">
           <img
             alt="team"
-            class="w-20 h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded mr-4"
+            class="flex-shrink-0 object-cover object-center w-20 h-20 mr-4 bg-gray-100 rounded"
             src="https://dummyimage.com/90x90" />
           <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">カクテル</h2>
+            <h2 class="font-medium text-gray-900 title-font">カクテル</h2>
             <p class="text-gray-500">300円</p>
           </div>
         </div>
       </div>
-      <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+      <div class="w-full p-2 lg:w-1/3 md:w-1/2">
+        <div class="flex items-center h-full p-4 border border-gray-200 rounded-lg">
           <img
             alt="team"
-            class="w-20 h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded mr-4"
+            class="flex-shrink-0 object-cover object-center w-20 h-20 mr-4 bg-gray-100 rounded"
             src="https://dummyimage.com/94x94" />
           <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">梅酒</h2>
+            <h2 class="font-medium text-gray-900 title-font">梅酒</h2>
             <p class="text-gray-500">300円</p>
           </div>
         </div>
       </div>
-      <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+      <div class="w-full p-2 lg:w-1/3 md:w-1/2">
+        <div class="flex items-center h-full p-4 border border-gray-200 rounded-lg">
           <img
             alt="team"
-            class="w-20 h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded mr-4"
+            class="flex-shrink-0 object-cover object-center w-20 h-20 mr-4 bg-gray-100 rounded"
             src="https://dummyimage.com/98x98" />
           <div class="flex-grow">
-            <h2 class="text-gray-900 title-font font-medium">カルピス</h2>
+            <h2 class="font-medium text-gray-900 title-font">カルピス</h2>
             <p class="text-gray-500">300円</p>
           </div>
         </div>
@@ -213,7 +213,7 @@ import Layout from '$components/layout/order/Layout.svelte';
       </ul>
     </div>
     <div class="flex-grow"></div>
-    <div class="font-bold text-xl py-4">合計金額: 900円</div>
+    <div class="py-4 text-xl font-bold">合計金額: 900円</div>
     <a href="/order/order_done" class="btn btn-info">注文する</a>
   </div>
 </div>
